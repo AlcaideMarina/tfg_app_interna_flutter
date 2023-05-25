29430486bb5b5aeff1e2a83317e90f8e740be6c9
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-25 21:58:10 +0200

    feat: Create fun to parse from order data to dborderfielddata

--- lib/utils/order_utils.dart
+++ lib/utils/order_utils.dart
@@ -1,6 +1,56 @@
 import 'package:hueveria_nieto_interna/data/models/local/bd_order_field_data.dart';
+import 'package:hueveria_nieto_interna/data/models/order_model.dart';
 
 class OrderUtils {
+
+  DBOrderFieldData orderDataToBDOrderModel(OrderModel orderData) {
+
+    Map<String, Map<String, num?>> order = orderData.order;
+    DBOrderFieldData dbOrderFieldData = DBOrderFieldData();
+
+    if (order.containsKey("xl_box")) {
+      Map<String, num?> xlBox = order["xl_box"]!;
+      dbOrderFieldData.xlBoxPrice = xlBox["price"];
+      dbOrderFieldData.xlBoxQuantity = xlBox["quantity"]!.toInt();
+    }
+    if (order.containsKey("xl_dozen")) {
+      Map<String, num?> xlDozen = order["xl_dozen"]!;
+      dbOrderFieldData.xlDozenPrice = xlDozen["price"];
+      dbOrderFieldData.xlDozenQuantity = xlDozen["quantity"]!.toInt();
+    }
+    if (order.containsKey("l_box")) {
+      Map<String, num?> lBox = order["l_box"]!;
+      dbOrderFieldData.lBoxPrice = lBox["price"];
+      dbOrderFieldData.lBoxQuantity = lBox["quantity"]!.toInt();
+    }
+    if (order.containsKey("l_dozen")) {
+      Map<String, num?> lDozen = order["l_dozen"]!;
+      dbOrderFieldData.lDozenPrice = lDozen["price"];
+      dbOrderFieldData.lDozenQuantity = lDozen["quantity"]!.toInt();
+    }
+    if (order.containsKey("m_box")) {
+      Map<String, num?> mBox = order["m_box"]!;
+      dbOrderFieldData.mBoxPrice = mBox["price"];
+      dbOrderFieldData.mBoxQuantity = mBox["quantity"]!.toInt();
+    }
+    if (order.containsKey("m_dozen")) {
+      Map<String, num?> mDozen = order["m_dozen"]!;
+      dbOrderFieldData.mDozenPrice = mDozen["price"];
+      dbOrderFieldData.mDozenQuantity = mDozen["quantity"]!.toInt();
+    }
+    if (order.containsKey("s_box")) {
+      Map<String, num?> sBox = order["s_box"]!;
+      dbOrderFieldData.sBoxPrice = sBox["price"];
+      dbOrderFieldData.sBoxQuantity = sBox["quantity"]!.toInt();
+    }
+    if (order.containsKey("s_dozen")) {
+      Map<String, num?> sDozen = order["s_dozen"]!;
+      dbOrderFieldData.sDozenPrice = sDozen["price"];
+      dbOrderFieldData.sDozenQuantity = sDozen["quantity"]!.toInt();
+    }
+    return dbOrderFieldData;
+
+  }
 
   String getOrderSummary(DBOrderFieldData dbOrderFieldData) {
 
