5cbef8670e2ff43f65a736b38a2a824f29a8b48c
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-25 21:37:32 +0200

    feat: Create db order field data structure

---
+++ lib/data/models/local/bd_order_field_data.dart
@@ -0,0 +1,45 @@
+class DBOrderFieldData {
+
+  final num? xlBoxPrice = null;
+  final num xlBoxQuantity = 0;
+  final num? xlDozenPrice = null;
+  final num xlDozenQuantity = 0;
+
+  final num? lBoxPrice = null;
+  final num lBoxQuantity = 0;
+  final num? lDozenPrice = null;
+  final num lDozenQuantity = 0;
+
+  final num? mBoxPrice = null;
+  final num mBoxQuantity = 0;
+  final num? mDozenPrice = null;
+  final num mDozenQuantity = 0;
+
+  final num? sBoxPrice = null;
+  final num sBoxQuantity = 0;
+  final num? sDozenPrice = null;
+  final num sDozenQuantity = 0;
+
+  DBOrderFieldData(
+    xlBoxPrice,
+    xlBoxQuantity,
+    xlDozenPrice,
+    xlDozenQuantity,
+
+    lBoxPrice,
+    lBoxQuantity,
+    lDozenPrice,
+    lDozenQuantity,
+
+    mBoxPrice,
+    mBoxQuantity,
+    mDozenPrice,
+    mDozenQuantity,
+
+    sBoxPrice,
+    sBoxQuantity,
+    sDozenPrice,
+    sDozenQuantity,
+  );
+
+}
