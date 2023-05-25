d0627b0d280477c4778da45e9e1c8e6b9a391940
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-25 21:19:59 +0200

    feat: Create parseTimestampToString fun

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hueveria_nieto_interna/utils/constants.dart';
+import 'package:intl/intl.dart';
 
 class Utils {
 
@@ -12,6 +14,16 @@ class Utils {
 
   int rolesStringToInt(String rolStr) {
     return Constants().roles[rolStr] ?? -1;
+  }
+
+  String? parseTimestmpToString(Timestamp? timestamp) {
+    if (timestamp == null) {
+      return null;
+    } else {
+      DateTime dateTime = timestamp.toDate();
+      String formattedDate = DateFormat('dd/MM/yyyy').format(dateTime);
+      return formattedDate;
+    }
   }
 
 }
