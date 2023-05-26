0e38cecf9627071c303dba71b1c8eb7b0a97074f
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-27 00:29:35 +0200

    fix: Correct dborderfielddata structure

--- lib/data/models/local/bd_order_field_data.dart
+++ lib/data/models/local/bd_order_field_data.dart
@@ -1,45 +1,151 @@
-class DBOrderFieldData {
 
-  num? xlBoxPrice;
-  num xlBoxQuantity = 0;
-  num? xlDozenPrice;
-  num xlDozenQuantity = 0;
+class DBOrderFieldData{
+  final double? xlBoxPrice;
+  final int? xlBoxQuantity;
+  final double? xlDozenPrice;
+  final int? xlDozenQuantity;
+  final double? lBoxPrice;
+  final int? lBoxQuantity;
+  final double? lDozenPrice;
+  final int? lDozenQuantity;
+  final double? mBoxPrice;
+  final int? mBoxQuantity;
+  final double? mDozenPrice;
+  final int? mDozenQuantity;
+  final double? sBoxPrice;
+  final int? sBoxQuantity;
+  final double? sDozenPrice;
+  final int? sDozenQuantity;
 
-  num? lBoxPrice;
-  num lBoxQuantity = 0;
-  num? lDozenPrice;
-  num lDozenQuantity = 0;
+  DBOrderFieldData(
+    this.xlBoxPrice, 
+    this.xlBoxQuantity, 
+    this.xlDozenPrice, 
+    this.xlDozenQuantity, 
+    this.lBoxPrice, 
+    this.lBoxQuantity, 
+    this.lDozenPrice, 
+    this.lDozenQuantity, 
+    this.mBoxPrice, 
+    this.mBoxQuantity, 
+    this.mDozenPrice, 
+    this.mDozenQuantity, 
+    this.sBoxPrice, 
+    this.sBoxQuantity, 
+    this.sDozenPrice, 
+    this.sDozenQuantity
+  );
 
-  num? mBoxPrice;
-  num mBoxQuantity = 0;
-  num? mDozenPrice;
-  num mDozenQuantity = 0;
+  Map<String, Map<String, num?>> toMap() {
+    
+    Map<String, Map<String, num?>> map = {};
 
-  num? sBoxPrice;
-  num sBoxQuantity = 0;
-  num? sDozenPrice;
-  num sDozenQuantity = 0;
+    if (xlDozenQuantity != null && xlDozenQuantity != 0) {
+      map['xl_dozen'] = {'price': xlDozenPrice, 'quantity': xlDozenQuantity};
+    }
+    if (xlBoxQuantity != null && xlBoxQuantity != 0) {
+      map['xl_box'] = {'price': xlBoxPrice, 'quantity': xlBoxQuantity};
+    }
+    if (lDozenQuantity != null && lDozenQuantity != 0) {
+      map['l_dozen'] = {'price': lDozenPrice, 'quantity': lDozenQuantity};
+    }
+    if (lBoxQuantity != null && lBoxQuantity != 0) {
+      map['l_box'] = {'price': lBoxPrice, 'quantity': lBoxQuantity};
+    }
+    if (mDozenQuantity != null && mDozenQuantity != 0) {
+      map['m_dozen'] = {'price': mDozenPrice, 'quantity': mDozenQuantity};
+    }
+    if (mBoxQuantity != null && mBoxQuantity != 0) {
+      map['m_box'] = {'price': mBoxPrice, 'quantity': mBoxQuantity};
+    }
+    if (sDozenQuantity != null && sDozenQuantity != 0) {
+      map['s_dozen'] = {'price': sDozenPrice, 'quantity': sDozenQuantity};
+    }
+    if (sBoxQuantity != null && sBoxQuantity != 0) {
+      map['s_box'] = {'price': sBoxPrice, 'quantity': sBoxQuantity};
+    }
 
-  DBOrderFieldData({
-    xlBoxPrice,
-    xlBoxQuantity = 0,
-    xlDozenPrice,
-    xlDozenQuantity = 0,
+    return map;
+  }
 
-    lBoxPrice,
-    lBoxQuantity = 0,
-    lDozenPrice,
-    lDozenQuantity = 0,
+  factory DBOrderFieldData.fromMap(Map<String, Map<String, num?>> json) {
 
-    mBoxPrice,
-    mBoxQuantity = 0,
-    mDozenPrice,
-    mDozenQuantity = 0,
+    int? xlBoxQuantityMap;
+    double? xlBoxPriceMap;
+    int? xlDozenQuantityMap;
+    double? xlDozenPriceMap;
+    int? lBoxQuantityMap;
+    double? lBoxPriceMap;
+    int? lDozenQuantityMap;
+    double? lDozenPriceMap;
+    int? mBoxQuantityMap;
+    double? mBoxPriceMap;
+    int? mDozenQuantityMap;
+    double? mDozenPriceMap;
+    int? sBoxQuantityMap;
+    double? sBoxPriceMap;
+    int? sDozenQuantityMap;
+    double? sDozenPriceMap;
 
-    sBoxPrice,
-    sBoxQuantity = 0,
-    sDozenPrice,
-    sDozenQuantity = 0,
-  });
+    if (json.containsKey("xl_box")) {
+      Map<String, dynamic> aux = json["xl_box"]!;
+      if (aux.containsKey("quantity")) xlBoxQuantityMap = aux["quantity"];
+      if (aux.containsKey("price")) xlBoxPriceMap = aux["price"];
+    }
+    if (json.containsKey("xl_dozen")) {
+      Map<String, dynamic> aux = json["xl_dozen"]!;
+      if (aux.containsKey("quantity")) xlDozenQuantityMap = aux["quantity"];
+      if (aux.containsKey("price")) xlDozenPriceMap = aux["price"];
+    }
+    if (json.containsKey("l_box")) {
+      Map<String, dynamic> aux = json["l_box"]!;
+      if (aux.containsKey("quantity")) lBoxQuantityMap = aux["quantity"];
+      if (aux.containsKey("price")) lBoxPriceMap = aux["price"];
+    }
+    if (json.containsKey("l_dozen")) {
+      Map<String, dynamic> aux = json["l_dozen"]!;
+      if (aux.containsKey("quantity")) lDozenQuantityMap = aux["quantity"];
+      if (aux.containsKey("price")) lDozenPriceMap = aux["price"];
+    }
+    if (json.containsKey("m_box")) {
+      Map<String, dynamic> aux = json["m_box"]!;
+      if (aux.containsKey("quantity")) mBoxQuantityMap = aux["quantity"];
+      if (aux.containsKey("price")) mBoxPriceMap = aux["price"];
+    }
+    if (json.containsKey("m_dozen")) {
+      Map<String, dynamic> aux = json["m_dozen"]!;
+      if (aux.containsKey("quantity")) mDozenQuantityMap = aux["quantity"];
+      if (aux.containsKey("price")) mDozenPriceMap = aux["price"];
+    }
+    if (json.containsKey("s_box")) {
+      Map<String, dynamic> aux = json["s_box"]!;
+      if (aux.containsKey("quantity")) sBoxQuantityMap = aux["quantity"];
+      if (aux.containsKey("price")) sBoxPriceMap = aux["price"];
+    }
+    if (json.containsKey("s_dozen")) {
+      Map<String, dynamic> aux = json["s_dozen"]!;
+      if (aux.containsKey("quantity")) sDozenQuantityMap = aux["quantity"];
+      if (aux.containsKey("price")) sDozenPriceMap = aux["price"];
+    }
 
+    return DBOrderFieldData(
+      xlBoxPriceMap,
+      xlBoxQuantityMap,
+      xlDozenPriceMap,
+      xlDozenQuantityMap,
+      lBoxPriceMap,
+      lBoxQuantityMap,
+      lDozenPriceMap,
+      lDozenQuantityMap,
+      mBoxPriceMap,
+      mBoxQuantityMap,
+      mDozenPriceMap,
+      mDozenQuantityMap,
+      sBoxPriceMap,
+      sBoxQuantityMap,
+      sDozenPriceMap,
+      sDozenQuantityMap,
+    );
+  }
+  
 }
