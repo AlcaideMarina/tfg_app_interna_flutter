c0b1711eeab5fbce98750061199cc810b21b70a2
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-27 15:02:02 +0200

    fix: Control nul in ordersummary fields

--- lib/utils/order_utils.dart
+++ lib/utils/order_utils.dart
@@ -60,14 +60,14 @@ class OrderUtils {
 
     List<String> list = [];
 
-    if (dbOrderFieldData.xlBoxQuantity != 0) list.add(dbOrderFieldData.xlBoxQuantity.toString() + " cajas XL");
-    if (dbOrderFieldData.xlDozenQuantity != 0) list.add(dbOrderFieldData.xlDozenQuantity.toString() + " docenas XL");
-    if (dbOrderFieldData.lBoxQuantity != 0) list.add(dbOrderFieldData.lBoxQuantity.toString() + " cajas L");
-    if (dbOrderFieldData.lDozenQuantity != 0) list.add(dbOrderFieldData.lDozenQuantity.toString() + " docenas L");
-    if (dbOrderFieldData.mBoxQuantity != 0) list.add(dbOrderFieldData.mBoxQuantity.toString() + " cajas M");
-    if (dbOrderFieldData.mDozenQuantity != 0) list.add(dbOrderFieldData.mDozenQuantity.toString() + " docenas M");
-    if (dbOrderFieldData.sBoxQuantity != 0) list.add(dbOrderFieldData.sBoxQuantity.toString() + " cajas S");
-    if (dbOrderFieldData.sDozenQuantity != 0) list.add(dbOrderFieldData.sDozenQuantity.toString() + " docenas S");
+    if (dbOrderFieldData.xlBoxQuantity != null && dbOrderFieldData.xlBoxQuantity != 0) list.add(dbOrderFieldData.xlBoxQuantity.toString() + " cajas XL");
+    if (dbOrderFieldData.xlDozenQuantity != null && dbOrderFieldData.xlDozenQuantity != 0) list.add(dbOrderFieldData.xlDozenQuantity.toString() + " docenas XL");
+    if (dbOrderFieldData.lBoxQuantity != null && dbOrderFieldData.lBoxQuantity != 0) list.add(dbOrderFieldData.lBoxQuantity.toString() + " cajas L");
+    if (dbOrderFieldData.lDozenQuantity != null && dbOrderFieldData.lDozenQuantity != 0) list.add(dbOrderFieldData.lDozenQuantity.toString() + " docenas L");
+    if (dbOrderFieldData.mBoxQuantity != null && dbOrderFieldData.mBoxQuantity != 0) list.add(dbOrderFieldData.mBoxQuantity.toString() + " cajas M");
+    if (dbOrderFieldData.mDozenQuantity != null && dbOrderFieldData.mDozenQuantity != 0) list.add(dbOrderFieldData.mDozenQuantity.toString() + " docenas M");
+    if (dbOrderFieldData.sBoxQuantity != null && dbOrderFieldData.sBoxQuantity != 0) list.add(dbOrderFieldData.sBoxQuantity.toString() + " cajas S");
+    if (dbOrderFieldData.sDozenQuantity != null && dbOrderFieldData.sDozenQuantity != 0) list.add(dbOrderFieldData.sDozenQuantity.toString() + " docenas S");
 
     String summary = "";
     for (var item in list) {
