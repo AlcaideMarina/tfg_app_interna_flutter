db91f47611c37ac3b8d2345b106737c585263624
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-29 02:56:59 +0200

    feat: Create newhensreourcepage

---
+++ lib/ui/views/hensresources/new_hens_resource_page.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:hueveria_nieto_interna/data/models/internal_user_model.dart';
+
+import '../../../custom/app_theme.dart';
+import '../../../custom/custom_sizes.dart';
+import '../../components/component_table_form_without_label.dart';
+import '../../components/component_text_input.dart';
+import '../../components/constants/hn_button.dart';
+
+class NewHensResourcePage extends StatefulWidget {
+  const NewHensResourcePage(this.currentUser, {Key? key}) : super(key: key);
+
+  final InternalUserModel currentUser;
+
+  @override
+  State<NewHensResourcePage> createState() => _NewHensResourcePageState();
+}
+
+class _NewHensResourcePageState extends State<NewHensResourcePage> {
+  late InternalUserModel currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    currentUser = widget.currentUser;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+            toolbarHeight: 56.0,
+            title: const Text(
+              'Detalle de trabajador',
+              style: TextStyle(
+                  color: AppTheme.primary, fontSize: CustomSizes.textSize24),
+            )),
+        body: SafeArea(
+          top: false,
+          child: SingleChildScrollView(
+            child: Container(
+                margin: const EdgeInsets.fromLTRB(24, 16, 24, 32),
+                child: Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      getComponentTableFormWithoutLable(getCells(), 
+                        columnWidhts: {
+                          0: const IntrinsicColumnWidth(),
+                        }),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      getButtonsComponent(),
+                      const SizedBox(
+                        height: 8,
+                      )
+                    ],
+                  ),
+                )),
+          ),
+        ));
+  }
+
+  Widget getComponentTableFormWithoutLable(List<TableRow> children,
+      {Map<int, TableColumnWidth>? columnWidhts}) {
+    double topMargin = 4;
+    double bottomMargin = 4;
+
+    return HNComponentTableFormWithoutLabel(
+      TableCellVerticalAlignment.middle,
+      children,
+      EdgeInsets.only(top: topMargin, bottom: bottomMargin),
+      columnWidths: columnWidhts,
+    );
+  }
+
+  List<TableRow> getCells() {
+    return [
+      TableRow(
+        children: [
+          Container(
+            child: Text("Fecha:"),
+            margin: const EdgeInsets.only(right: 16),
+          ),
+          Container(
+              height: 40,
+              margin: const EdgeInsets.only(left: 8, bottom: 0),
+              child: HNComponentTextInput(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                textInputType: const TextInputType.numberWithOptions(),
+                initialValue: "TODO",
+                isEnabled: true,
+              ),
+            ),
+        ]
+      ),
+      TableRow(
+        children: [
+          Container(
+            child: Text("Cantidad:"),
+            margin: const EdgeInsets.only(right: 16, top: 4),
+          ),
+          //TODO: Controlador de los campos de las naves tiene que venir del onchange de aquí
+          Container(
+              height: 40,
+              margin: const EdgeInsets.only(left: 8, bottom: 0, top: 4),
+              child: HNComponentTextInput(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                textInputType: const TextInputType.numberWithOptions(),
+                isEnabled: true,
+              ),
+            ),
+        ]
+      ),
+      TableRow(
+        children: [
+          Container(
+            child: Text("Nave A:"),
+            margin: const EdgeInsets.only(right: 16, top: 4, left: 16),
+          ),
+          Container(
+              height: 40,
+              margin: const EdgeInsets.only(left: 8, bottom: 0, top: 4),
+              child: HNComponentTextInput(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                textInputType: const TextInputType.numberWithOptions(),
+                isEnabled: true,
+              ),
+            ),
+        ]
+      ),
+      TableRow(
+        children: [
+          Container(
+            child: Text("Nave B:"),
+            margin: const EdgeInsets.only(right: 16, top: 4, left: 16),
+          ),
+          Container(
+              height: 40,
+              margin: const EdgeInsets.only(left: 8, bottom: 0, top: 4),
+              child: HNComponentTextInput(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                textInputType: const TextInputType.numberWithOptions(),
+                isEnabled: true,
+              ),
+            ),
+        ]
+      ),
+      TableRow(
+        children: [
+          Container(
+            child: Text("Precio total:"),
+            margin: const EdgeInsets.only(right: 16, top: 4),
+          ),
+          Container(
+              height: 40,
+              margin: const EdgeInsets.only(left: 8, bottom: 0, top: 4),
+              child: HNComponentTextInput(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                textInputType: const TextInputType.numberWithOptions(),
+                isEnabled: true,
+              ),
+            ),
+        ]
+      ),
+      
+    ];
+  }
+
+  Widget getButtonsComponent() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        children: [
+          HNButton(ButtonTypes.blackWhiteBoldRoundedButton)
+              .getTypedButton('Guardar', null, null, () {}, null),
+          const SizedBox(
+            height: 8,
+          ),
+          HNButton(ButtonTypes.redWhiteBoldRoundedButton)
+              .getTypedButton(
+                'Cancelar', 
+                null, 
+                null, 
+                () {}, 
+                null, 
+              ),
+        ])
+    );
+  }
+  
+}
