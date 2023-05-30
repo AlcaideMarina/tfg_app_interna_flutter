21f29d7780e21566baaf0139d81ca1f6c3c34139
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-05-30 19:57:36 +0200

    feat: Create feed detail page

---
+++ lib/ui/views/feedresources/feed_resources_detail_page.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:hueveria_nieto_interna/data/models/feed_resources_model.dart';
+import 'package:hueveria_nieto_interna/data/models/internal_user_model.dart';
+
+import '../../../custom/app_theme.dart';
+import '../../../custom/custom_sizes.dart';
+import '../../../utils/Utils.dart';
+import '../../components/component_table_form_without_label.dart';
+import '../../components/component_text_input.dart';
+import '../../components/constants/hn_button.dart';
+
+class FeedResourcesDetailPage extends StatefulWidget {
+  const FeedResourcesDetailPage(this.currentUser, this.feedModel,  {Key? key}) : super(key: key);
+
+  final InternalUserModel currentUser;
+  final FeedResourcesModel feedModel;
+
+  @override
+  State<FeedResourcesDetailPage> createState() => _FeedResourcesDetailPageState();
+}
+
+class _FeedResourcesDetailPageState extends State<FeedResourcesDetailPage> {
+  late InternalUserModel currentUser;
+  late FeedResourcesModel feedResourcesModel;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    currentUser = widget.currentUser;
+    feedResourcesModel = widget.feedModel;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+            toolbarHeight: 56.0,
+            title: const Text(
+              'Detalle - Pienso',
+              style: TextStyle(
+                  color: AppTheme.primary, fontSize: CustomSizes.textSize24),
+            )),
+        body: SafeArea(
+          top: false,
+          child: SingleChildScrollView(
+            child: Container(
+                margin: const EdgeInsets.fromLTRB(24, 16, 24, 32),
+                child: Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      getComponentTableFormWithoutLable(getCells(), 
+                        columnWidhts: {
+                          0: const IntrinsicColumnWidth(),
+                        }),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      getButtonsComponent(),
+                      const SizedBox(
+                        height: 8,
+                      )
+                    ],
+                  ),
+                )),
+          ),
+        ));
+  }
+
+  Widget getComponentTableFormWithoutLable(List<TableRow> children,
+      {Map<int, TableColumnWidth>? columnWidhts}) {
+    double topMargin = 4;
+    double bottomMargin = 4;
+
+    return HNComponentTableFormWithoutLabel(
+      TableCellVerticalAlignment.middle,
+      children,
+      EdgeInsets.only(top: topMargin, bottom: bottomMargin),
+      columnWidths: columnWidhts,
+    );
+  }
+
+  List<TableRow> getCells() {
+    return [
+      TableRow(
+        children: [
+          Container(
+            child: Text("Fecha:"),
+            margin: const EdgeInsets.only(right: 16),
+          ),
+          Container(
+            child: Text(Utils().parseTimestmpToString(feedResourcesModel.expenseDatetime) ?? ""),
+            margin: const EdgeInsets.only(left: 16),
+          ),
+        ]
+      ),
+      TableRow(
+        children: [
+          Container(
+            child: Text("Cantidad (kg):"),
+            margin: const EdgeInsets.only(right: 16, top: 4),
+          ),
+          Container(
+              height: 40,
+              margin: const EdgeInsets.only(left: 8, bottom: 0, top: 4),
+              child: HNComponentTextInput(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                textInputType: const TextInputType.numberWithOptions(),
+                isEnabled: false,
+                labelText: feedResourcesModel.kilos.toString(),
+              ),
+            ),
+        ]
+      ),
+      TableRow(
+        children: [
+          Container(
+            child: Text("Precio total:"),
+            margin: const EdgeInsets.only(right: 16, top: 4),
+          ),
+          Container(
+              height: 40,
+              margin: const EdgeInsets.only(left: 8, bottom: 0, top: 4),
+              child: HNComponentTextInput(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                textInputType: const TextInputType.numberWithOptions(),
+                isEnabled: false,
+                labelText: feedResourcesModel.totalPrice.toString(),
+              ),
+            ),
+        ]
+      ),
+      
+    ];
+  }
+
+  Widget getButtonsComponent() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        children: [
+          HNButton(ButtonTypes.blackWhiteBoldRoundedButton)
+              .getTypedButton('Modificar', null, null, () {}, null),
+          const SizedBox(
+            height: 8,
+          ),
+          HNButton(ButtonTypes.redWhiteBoldRoundedButton)
+              .getTypedButton(
+                'Eliminar', 
+                null, 
+                null, 
+                () {}, 
+                null, 
+              ),
+        ])
+    );
+  }
+}
