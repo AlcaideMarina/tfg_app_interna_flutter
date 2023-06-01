43014209898c7cbbbe7071fe74e50918d970c2c5
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-01 04:14:50 +0200

    feat: Add some fun in farmutils

---
+++ lib/utils/farm_utils.dart
@@ -0,0 +1,88 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hueveria_nieto_interna/data/models/fpc_model.dart';
+
+import '../data/models/local/monthly_fpc_container_data.dart';
+import 'Utils.dart';
+
+class FarmUtils {
+
+  List<FPCModel> getFPCModel(List? mapList) {
+    List<FPCModel> list = [];
+
+    if (mapList != null) {
+      for (var item in mapList) {
+        FPCModel fpcModel = FPCModel.fromMap(item.data(), item.id);
+        if (!fpcModel.deleted) {
+          list.add(
+            FPCModel(
+              fpcModel.acceptedEggs, 
+              fpcModel.bestBeforeDatetime, 
+              fpcModel.createdBy, 
+              fpcModel.creationDatetime, 
+              fpcModel.deleted, 
+              fpcModel.issueDatetime, 
+              fpcModel.layingDatetime, 
+              fpcModel.lot, 
+              fpcModel.packingDatetime, 
+              fpcModel.rejectedEggs, 
+              fpcModel.documentId
+            )
+          );
+        }
+      }
+    }
+    return list;
+  }
+
+  List<MonthlyFPCContainerData> getMonthlyFPCModelFromFPCModelList(List<FPCModel> fpcModelList) {
+    List<MonthlyFPCContainerData> list = [];
+
+    fpcModelList.sort((a, b) => b.layingDatetime.compareTo(a.layingDatetime));
+
+    if (fpcModelList.isNotEmpty) {
+
+      FPCModel firstFPC = fpcModelList[0];
+      DateTime firstDate = firstFPC.layingDatetime.toDate();
+      
+      String m = firstDate.month.toString();
+      while(m.length < 2) {
+        m = '0' + m;
+      }
+      String y = firstDate.year.toString();
+      while(y.length < 4) {
+        y = '0' + y;
+      }
+
+      // Creamos fecha inicial y final
+      Timestamp initDateTimestamp = Utils().parseStringToTimestamp('01/$m/$y');
+      Timestamp endDateTimestamp = Timestamp.fromDate(
+        Utils().addToDate(initDateTimestamp.toDate(), monthsToAdd: 1)
+      );
+
+      List<FPCModel> monthlyFPCDataList = [];
+      for (FPCModel item in fpcModelList) {
+        if(initDateTimestamp.compareTo(item.layingDatetime) > 1) {
+          MonthlyFPCContainerData monthlyFPCContainerData = MonthlyFPCContainerData(
+            initDateTimestamp, endDateTimestamp, monthlyFPCDataList);
+          list.add(monthlyFPCContainerData);
+
+          endDateTimestamp = initDateTimestamp;
+          initDateTimestamp = Timestamp.fromDate(Utils().addToDate(initDateTimestamp.toDate(), monthsToAdd: -1));
+          monthlyFPCDataList = [];
+        }
+        monthlyFPCDataList.add(item);
+
+        if (fpcModelList.last == item) {
+          MonthlyFPCContainerData monthlyFPCContainerData = MonthlyFPCContainerData(
+            initDateTimestamp, endDateTimestamp, monthlyFPCDataList);
+          list.add(monthlyFPCContainerData);
+
+          endDateTimestamp = initDateTimestamp;
+          initDateTimestamp = Timestamp.fromDate(Utils().addToDate(initDateTimestamp.toDate(), monthsToAdd: -1));
+        }
+      }
+    }
+    return list;
+  }
+
+}
