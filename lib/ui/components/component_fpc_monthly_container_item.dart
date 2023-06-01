c471ae94bf9246761ae8aa33341a67aa6480b3ce
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-01 04:14:24 +0200

    feat: Add monthlydata in monthlycomponent

--- lib/ui/components/component_fpc_monthly_container_item.dart
+++ lib/ui/components/component_fpc_monthly_container_item.dart
@@ -1,24 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:hueveria_nieto_interna/data/models/local/monthly_fpc_container_data.dart';
 
 import '../../custom/custom_colors.dart';
 import '../../data/models/fpc_model.dart';
 import '../../utils/Utils.dart';
 import '../../values/image_routes.dart';
 
-class HNCompoentnFPCMonthlyContainerItem extends StatelessWidget {
+class HNComponentFPCMonthlyContainerItem extends StatelessWidget {
 
-  final Timestamp initDate;
-  final Timestamp endDate;
-  final List<FPCModel> list;
+  final MonthlyFPCContainerData monthlyFPCContainerData;
   final Function()? onTap;
 
-  HNCompoentnFPCMonthlyContainerItem(
-    this.initDate, 
-    this.endDate, 
-    this.list, 
-    this.onTap
-  );
+  const HNComponentFPCMonthlyContainerItem(
+    this.monthlyFPCContainerData, {Key? key, this.onTap}
+  ) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,12 +24,12 @@ class HNCompoentnFPCMonthlyContainerItem extends StatelessWidget {
       padding: const EdgeInsets.all(16),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(Utils().parseTimestmpToString(initDate, dateFormat: "MMMM, yyyy") ?? ""),
+              Text(Utils().parseTimestmpToString(monthlyFPCContainerData.initDate, dateFormat: "MMMM, yyyy") ?? ""),
             ],
           ),
           onTap != null ? Image.asset(
