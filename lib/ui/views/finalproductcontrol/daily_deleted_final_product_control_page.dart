946a98776c406c75ddf5e4d81534b370c6f302ce
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-02 02:16:30 +0200

    feat: Create daily deleted fpc page

---
+++ lib/ui/views/finalproductcontrol/daily_deleted_final_product_control_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:hueveria_nieto_interna/data/models/internal_user_model.dart';
+import 'package:hueveria_nieto_interna/data/models/local/monthly_fpc_container_data.dart';
+
+import '../../../custom/app_theme.dart';
+import '../../../custom/custom_sizes.dart';
+import '../../components/component_daily_final_product_control.dart';
+import '../../components/component_panel.dart';
+
+class DailyDeletedFinalProdcutControlPage extends StatefulWidget {
+  const DailyDeletedFinalProdcutControlPage(
+      this.currentUser, this.monthlyFPCContainerData,
+      {Key? key})
+      : super(key: key);
+
+  final InternalUserModel currentUser;
+  final MonthlyFPCContainerData monthlyFPCContainerData;
+
+  @override
+  State<DailyDeletedFinalProdcutControlPage> createState() =>
+      _DailyDeletedFinalProdcutControlPageState();
+}
+
+class _DailyDeletedFinalProdcutControlPageState
+    extends State<DailyDeletedFinalProdcutControlPage> {
+  late InternalUserModel currentUser;
+  late MonthlyFPCContainerData monthlyFPCContainerData;
+
+  @override
+  void initState() {
+    super.initState();
+    currentUser = widget.currentUser;
+    monthlyFPCContainerData = widget.monthlyFPCContainerData;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+          toolbarHeight: 56.0,
+          title: const Text(
+            "Todos los pedidos",
+            style: TextStyle(
+                color: AppTheme.primary, fontSize: CustomSizes.textSize24),
+          )),
+      body: Container(
+          child: monthlyFPCContainerData.list.isNotEmpty
+              ? ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  itemCount: monthlyFPCContainerData.list.length,
+                  itemBuilder: (context, i) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 8),
+                      child: HNComponentDailyFPC(
+                        monthlyFPCContainerData.list[i],
+                        onTap: () {},
+                      ),
+                    );
+                  })
+              : Container(
+                  margin: const EdgeInsets.fromLTRB(32, 56, 32, 8),
+                  child: const HNComponentPanel(
+                    title: 'No hay recursos',
+                    text:
+                        "No hay registro de recursos de cajas y cartones no eliminados en la base de datos.",
+                  ))),
+    );
+  }
+}
