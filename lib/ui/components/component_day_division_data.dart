7060bd93f25afbf6bbe7cdae28c73efd54dece58
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-03 22:49:48 +0200

    feat: Create day division component

---
+++ lib/ui/components/component_day_division_data.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+import '../../custom/custom_colors.dart';
+import '../../utils/Utils.dart';
+import '../../values/image_routes.dart';
+
+class HNComponentDayDivisionData extends StatelessWidget {
+  const HNComponentDayDivisionData(this.timestamp, {Key? key, this.onTap}) : super(key: key);
+
+  final Timestamp timestamp;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                child: Container(
+                  margin: const EdgeInsets.only(right: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(Utils().parseTimestmpToString(timestamp) ?? "?"),
+                    ],
+                  ),
+                ),
+              ),
+              onTap != null ? Image.asset(
+                ImageRoutes.getRoute('ic_next_arrow'), 
+                width: 16,
+                height: 24,)
+              : Container()
+            ],
+          ),
+          decoration: BoxDecoration(
+            color: CustomColors.redGraySecondaryColor,
+            border: Border.all(
+              color: CustomColors.redGraySecondaryColor,
+            ),
+            borderRadius: const BorderRadius.all(Radius.circular(20))
+          ),
+        )
+    );
+  }
+}
