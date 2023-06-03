a60b43f2dd60efa0f3024c5658746862fe74e668
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-04 00:04:12 +0200

    feat: Create weeklymcs data model

---
+++ lib/data/models/local/weekly_monitoring_company_situation_data.dart
@@ -0,0 +1,13 @@
+class WeeklyMonitoringCompanySituationData {
+  
+  final double averageNumberHensWeek;
+  final int weeklyLaying;
+  final double weeklyLayingRate;
+
+  WeeklyMonitoringCompanySituationData(
+    this.averageNumberHensWeek, 
+    this.weeklyLaying, 
+    this.weeklyLayingRate
+  );
+
+}
