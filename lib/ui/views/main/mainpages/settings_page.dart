1e63d2941a7946f260d8fec849ea8787c165332e
Marina Alcaide <marinaalcaidec99@gmail.com>
2023-06-16 01:50:30 +0200

    feat: Create settings page

---
+++ lib/ui/views/main/mainpages/settings_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../../../../custom/app_theme.dart';
+import '../../../../custom/custom_sizes.dart';
+import '../../../../data/models/internal_user_model.dart';
+import '../../../components/component_list_data.dart';
+import '../../../components/menu/lateral_menu.dart';
+
+class SettingsPage extends StatefulWidget {
+  SettingsPage(this.currentUser, {Key? key}) : super(key: key);
+
+  final InternalUserModel currentUser;
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  late InternalUserModel currentUser;
+
+  @override
+  void  initState() {
+    super.initState();
+    currentUser = widget.currentUser;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double _width = MediaQuery.of(context).size.width;
+    final double _height = MediaQuery.of(context).size.height;
+
+    GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.white,
+      drawer: LateralMenu(currentUser),
+      appBar: AppBar(
+          toolbarHeight: 56.0,
+          title: const Text(
+            "Ajustes",
+            style: TextStyle(
+                color: AppTheme.primary, fontSize: CustomSizes.textSize24),
+          )),
+      body: ListView(
+          children: const [
+            HNComponentListData(
+              "Cambiar contraseña", 
+              true,
+            ),
+            HNComponentListData(
+              "Cambiar contraseña", 
+              false,
+            ),
+          ],
+        )
+    );
+  }
+}
